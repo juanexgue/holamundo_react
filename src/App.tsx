941f50e64c13482ed8c7e45dcbe11f90050514df
Juan Guevara M. <juanexgue@gmail.com>
2025-01-28 00:58:11 -0500

    suma en inputs

--- src/App.tsx
+++ src/App.tsx
@@ -4,44 +4,49 @@ import List from "./components/List";
 import { useState } from "react";
 
 
-function realizarOperacion(e){
-  e.preventDefault();
-  const v1 = parseInt(e.target.valor1.value);
-  const v2 = parseInt(e.target.valor2.value);
-  const suma = v1 + v2;
-  alert("La suma es :"+suma)
-}
-
-
 function App() {
+
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [sum, setSum] = useState(0);
+
+  const handleSum = () => {
+    const value1 = parseFloat(input1) || 0;
+    const value2 = parseFloat(input2) || 0;
+    setSum(value1 + value2);
+  };
 
   return (
 
     <>
-    <form onSubmit={realizarOperacion}>
+    
     <div className="input-group mb-3">
       <span className="input-group-text">S/.</span>
-      <input type="number" name="valor1" className="form-control" placeholder="Monto cuota 1"
+      <input value={input1} onChange={(e)=> setInput1(e.target.value)}
+      type="number" name="valor1" 
+      className="form-control" placeholder="Monto cuota 1"
         aria-label="Username" aria-describedby="basic-addon1" />
     </div>
       <div className="input-group mb-3">
       <span className="input-group-text">S/.</span>
-        <input type="number" name="valor2" className="form-control" placeholder="Monto cuota 2"
+        <input value={input2} onChange={(e)=> setInput2(e.target.value)}
+        type="number" name="valor2" 
+        className="form-control" placeholder="Monto cuota 2"
           aria-label="Username" aria-describedby="basic-addon1" />
 
       </div>
       <div className="input-group mb-3">
-      <button type="submit" className="btn btn-primary">Sumar</button>
+      <button onClick ={handleSum} type="submit" className="btn btn-primary">Sumar</button>
 
       </div>
 
       <div className="input-group mb-3">
       <span className="input-group-text">S/.</span>
-        <input type="text" className="form-control" placeholder="Monto Total"
+        <input value={sum} type="text" className="form-control" placeholder="Monto Total" 
           aria-label="Username" aria-describedby="basic-addon1" />
 
       </div>
-      </form>
+    
     </>
   )
 
